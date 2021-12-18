874b53514179cb7885e0c6a01d568f874187f091
Ngoc Bich Nguyen <ngocbichnguyen1603@gmail.com>
2021-12-18 12:26:13 +0700

    2nd version: add C:\spark\spark-3.1.2-bin-hadoop2.7

--- collaborativefiltering_recommender_page.py
+++ collaborativefiltering_recommender_page.py
@@ -10,6 +10,9 @@ import findspark
 import os
 import streamlit as st
 import io
+
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
+os.environ["SPARK_HOME"] = "/content/spark-2.4.0-bin-hadoop2.7"
 findspark.init()
 
 
